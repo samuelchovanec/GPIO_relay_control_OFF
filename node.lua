372cf3d9cd68dd105333bdab6bfe5000dfb4423d
samuelchovanec <samuel.chovanec@gmail.com>
2023-06-21 18:36:47 +0200

    Add files via upload

---
+++ node.lua
@@ -0,0 +1,24 @@
+-- gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+-- local font = resource.load_font "font.ttf"
+
+-- function node.render()
+--     font:write(250, 300, "Hello world", 64, 1,1,1,1)
+--     gl.clear(0, 1, 0, 1) -- green
+-- end
+
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+util.no_globals()
+
+local on = false
+
+util.data_mapper{
+    state = function(state)
+        on = state == '1'
+    end,
+}
+
+function node.render()
+    gl.clear(0, 1, 0, 1) -- green    
+end
